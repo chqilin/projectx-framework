ed5501b38dacdb651b5c04448c86db9fcb9386a5
Qilin <qilin.cheng@foxmail.com>
2020-04-23 00:50:34 +0800

    @1.1.2 NEW: ILRuntime

---
+++ Assets/_TEST/ILRuntimeTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ILRuntimeTest : MonoBehaviour
+{
+    ILRuntime.Runtime.Enviorment.AppDomain appdomain = null;
+
+    void Start()
+    {
+        appdomain = new ILRuntime.Runtime.Enviorment.AppDomain();
+    }
+}
